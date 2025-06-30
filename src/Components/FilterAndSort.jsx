7b516d445605b6ab96b61152e9a690c4848cc75d
Md. Rashidul Islam <jrashidulislam0103@gmail.com>
2025-06-30 16:11:02 +0600

    image error solve

--- src/Components/FilterAndSort.jsx
+++ src/Components/FilterAndSort.jsx
@@ -18,7 +18,7 @@ function FilterAndSort({
         <input
           id="search"
           type="text"
-          placeholder="Search by title or description..."
+          placeholder="Search by product name or description..."
           className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
           value={searchText}
           onChange={(e) => onSearchChange(e.target.value)}
